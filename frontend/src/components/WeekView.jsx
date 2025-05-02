a5850e3edd78e1c989e46cfe994ef0e3a1e17ebd
Daniel Jang <83668787+ndjn3979@users.noreply.github.com>
2025-05-01 20:08:19 -0700

    5/1 Changelog - Daniel

--- frontend/src/components/WeekView.jsx
+++ frontend/src/components/WeekView.jsx
@@ -11,12 +11,30 @@ const daysOfWeek = [
 ];
 
 const WeekView = ({ chores }) => {
+  {/* 
+  Controls how weekdays appear in a grid
+    - original Tailwind className = 'grid grid-cols-4 gap-4'
+    - Daniel - changed to 7 columns for a traditional calendar view
+  */}
+
+  {/* Daniel - Added validChores filter to eliminate loading error w/ chores missing fields */}
+  const validChores = Array.isArray(chores) 
+    ? chores.filter(chore => chore && typeof chore === 'object' && chore.day)
+    : [];
+    
+
   return (
-    <div className='grid grid-cols-4 gap-4'>
+    <div className='grid grid-cols-7 gap-4'>
       {daysOfWeek.map((day) => {
-        const choresForDay = chores.filter(
+        const choresForDay = validChores.filter(
           (chore) => chore.day.toLowerCase() === day
-        );
+        );  
+        {/*
+          Previously, this part was causing an error:
+            - (chore) => chore.day.toLowerCase() === day
+            - Uncaught TypeError: Cannot read properties of null (reading 'toLowerCase')
+            - Daniel - added a filter function above the original logic to ensure proper loading of website
+        */}
 
         return <DayCard key={day} day={day} chores={choresForDay} />;
       })}
